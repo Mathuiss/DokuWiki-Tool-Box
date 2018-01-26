c4d15abaa7d53013e922c2d1488d732d2492e1a9
Mathuis G <mathijsgeerlings@gmail.com>
2018-01-26 16:51:47 +0100

    XmlParser Prototype (Should Hypothetically work)

--- DokuWikiToolBox/DokuWikiToolBox/XmlParser.cs
+++ DokuWikiToolBox/DokuWikiToolBox/XmlParser.cs
@@ -16,26 +16,70 @@ namespace DokuWikiToolBox
 
         public void GetNodes(DocObject doc, ref List<XmlNode> nodeList)
         {
+            
             string[] xml = doc.Lines.ToArray();
 
             for (int i = 0; i < xml.Length; i++)
             {
+                //New node detected
                 if (xml[i].Contains("<w:pPr"))
                 {
+                    var node = new XmlNode();
+
+                    //Get type
                     if (xml[i + 1].Contains("<w:pStyle"))
                     {
-                        Console.WriteLine(GetValue(xml[i + 1], "\""));
+                        node.Type = GetType(xml[i + 1], "\"");
+
+                        //Fast forward to value
+                        while (!xml[i].Contains("<w:t"))
+                        {
+                            i++;
+                        }
+
+                        if (xml[i].Contains("<w:t"))
+                            node.Value = GetValue(xml[i], "<w:t");
                     }
                 }
             }
         }
 
-        public string GetValue(string input, string target)
+        public string GetType(string input, string target)
         {
             int first = input.IndexOf(target) + 1;
             input = input.Remove(0, first);
             int last = input.LastIndexOf(target);
             input = input.Remove(last, input.Length - last); //Because it's a 0 based array
+            return input;
+        }
+
+        public string GetValue(string input, string target)
+        {
+            char[] line = input.ToCharArray();
+            bool hasReadValue = false;
+            int first = 0;
+            int last = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '>')
+                {
+                    first = i;
+                    hasReadValue = true;
+                }
+                else if (line[i] == '<' && hasReadValue)
+                {
+                    last = i;
+                    break;
+                }
+            }
+
+            if (first == 0 || last == 0)
+                throw new Exception();
+
+            input = input.Remove(0, first);
+            input = input.Remove(last, input.Length - last); //Because it's a 0 based array
+
             return input;
         }
     }
